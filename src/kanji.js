009e5c2c5e2c929641520913f0a1de54e07b8730
Marques <MBatoon1996@gmail.com>
2021-04-17 16:42:46 -1000

    shuffle button for yomu and kaku sections

---
+++ src/kanji.js
@@ -0,0 +1,22 @@
+const kanji = {
+    kaku: {
+
+        初めて: { 読み方: ['はじめて'] },
+        石山: { 読み方: ['いしやま'] },
+        遅い: { 読み方: ['おそい'] },
+        関係: { 読み方: ['関係'] },
+
+    },
+
+    yomerebaii: {
+
+        紹介:{ 読み方: ['しょうかい'], form: '名詞、スル動詞、ノ形容詞', 意味: "Introduction" },
+        宿題:{ 読み方: ['しゅくだい'], form: '名詞', 意味: "Homework" },
+        感想:{ 読み方: ['かんそう'], form: '名詞', 意味: "one's thoughts/imporessions" },
+        文集:{ 読み方: ['ぶんしゅう'], form: '名詞', 意味: 'Collection of essays' },
+        悔やむ:{ 読み方: ['くやむ'], form: '五段動詞、他動詞', 意味: 'To regret' },
+
+    }
+}
+
+export default kanji;
