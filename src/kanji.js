e2d832ed612faa73dd58d0bc8b5cfd1a43d437a1
Marques <MBatoon1996@gmail.com>
2021-04-28 05:52:02 -1000

    added ch5 kanji yomerebaii

--- src/kanji.js
+++ src/kanji.js
@@ -104,6 +104,32 @@ const kanji = {
         家庭:{ 読み方: ['かてい'], form: '名詞', 意味: "family; home" },
         希望者:{ 読み方: ['きぼうしゃ'], form: '名詞', 意味: "applicant; person interested in doing something" },
         住宅事情:{ 読み方: ['じゅうたくじじょう'], form: '名詞', 意味: "housing conditions/situation" },
+        // Chapter 4
+        許可:{ 読み方: ['きょか'], form: '名詞', 意味: "permission" },
+        財布:{ 読み方: ['さいふ'], form: '名詞', 意味: "wallet" },
+        背:{ 読み方: ['せ'], form: '名詞', 意味: "one's height" },
+        弁護士:{ 読み方: ['べんごし'], form: '名詞', 意味: "lawyer" },
+        珍しい:{ 読み方: ['めずらしい'], form: 'イ形容詞', 意味: "rare; unusual" },
+        全然:{ 読み方: ['ぜんぜん（～ない）'], form: '副詞', 意味: "(not) at all" },
+        州都:{ 読み方: ['しゅうと'], form: '名詞', 意味: "state capital" },
+        知事:{ 読み方: ['ちじ'], form: '名詞', 意味: "governor" },
+        皆さん:{ 読み方: ['みなさん'], form: '名詞', 意味: "all of you; everyone" },
+        求める:{ 読み方: ['もとめる'], form: '一段動詞、他動詞', 意味: "to request; to ask for" },
+        偶然:{ 読み方: ['ぐうぜん'], form: 'ナ形容詞、ノ形容詞、副詞', 意味: "chance; coincidence; by chance" },
+        暖かい:{ 読み方: ['あたｔかい'], form: 'イ形容詞', 意味: "pleasantly warm [with reference to weather]" },
+        紅葉:{ 読み方: ['こうよう'], form: '名詞、スル動詞', 意味: "fall colors (of leaves)" },
+        面白い:{ 読み方: ['おもしろい'], form: 'イ形容詞', 意味: "interesting, amusing" },
+        経験:{ 読み方: ['けいけん'], form: '名詞、する動詞', 意味: "experience" },
+        済む:{ 読み方: ['（～ないで）すむ'], form: '五段動詞、自動詞', 意味: "to get by without ~ing" },
+        携帯:{ 読み方: ['けいたい'], form: '名詞', 意味: "cellular (phone)" },
+        熱心:{ 読み方: ['ねっしん（に）'], form: 'ナ形容詞', 意味: "eager(ly); ardent(ly; enthusiastic(ly)" },
+        打つ:{ 読み方: ['うつ'], form: '五段動詞、他動詞', 意味: "to type (messages)" },
+        料理:{ 読み方: ['りょうり'], form: '名詞、スル動詞', 意味: "cooking; cuisine" },
+        物:{ 読み方: ['もの'], form: '名詞', 意味: "thing" },
+        体重:{ 読み方: ['たいじゅう'], form: '名詞', 意味: "body weight" },
+        足りる:{ 読み方: ['たりる'], form: '一段動詞、自動詞', 意味: "to be sufficient; to be enough" },
+        伝える:{ 読み方: ['つたえる'], form: '一段動詞、他動詞', 意味: "to convey; to report; to communicate" },
+        // Chapter 5
 
     }
 }
