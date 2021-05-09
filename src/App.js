e8f7fd8efb59b85856e197adbb63c33b82d17017
Marques <MBatoon1996@gmail.com>
2021-05-09 01:31:14 -1000

    ch5 kaku button and chars added

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Layout from './Layout';
 import Home from './Home';
 import Yomu from './Yomu';
-import Kaku from './Kaku';
 import './App.css';
 
 const App = () => {
@@ -13,7 +12,6 @@ const App = () => {
         <Switch>
           <Route path ="/" exact component={Home} />
           <Route path="/kanji" component={Yomu} />
-          <Route path="/書くのを覚える" component={Kaku} />
           <Route render={() => <h1>404 Not Found</h1>} />
         </Switch>
       </Layout>
