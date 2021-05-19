9da39527fede28c426f412c4fec6b1fc812200bb
Marques <MBatoon1996@gmail.com>
2021-05-19 01:47:30 -1000

    ch12 done

--- src/kanji.js
+++ src/kanji.js
@@ -304,6 +304,34 @@ const kanji = {
         // Chapter 9
 
         // : { 読み方: [''], form: '', 意味: "" },
+
+        // Chapter 12
+        びょうじょう: { 読み方: ['病状'], form: '名詞', 意味: "the condition of a disease or illness; patient's condition" },
+        かおいろ: { 読み方: ['顔色'], form: '名詞', 意味: "complexion; one's color" },
+        いしゃ: { 読み方: ['医者'], form: '名詞', 意味: "(medical) doctor" },
+        ちず: { 読み方: ['地図'], form: '名詞', 意味: "map" },
+        ねつ: { 読み方: ['熱'], form: '名詞', 意味: "fever" },
+        さむけ: { 読み方: ['寒気がする'], form: '名詞', 意味: "chill; the shivers" },
+        しょくよく: { 読み方: ['食欲'], form: '名詞', 意味: "appetite" },
+        くすり: { 読み方: ['薬'], form: '名詞', 意味: "medicine" },
+        なおる: { 読み方: ['治る'], form: '五段動詞、自動詞', 意味: "to get better; to recover from illness" },
+        おきる: { 読み方: ['起きる'], form: '一段動詞、自動詞', 意味: "to get up" },
+        しかめっつら: { 読み方: ['しかめっ面'], form: '名詞、ノ形容詞', 意味: "grimace; frown" },
+        けさ: { 読み方: ['今朝'], form: '名詞', 意味: "this morning" },
+        あく: { 読み方: ['空く'], form: '五段動詞、自動詞', 意味: "to become vacant" },
+        うんがいい: { 読み方: ['運がいい'], form: '表現、イ形容詞', 意味: "lucky" },
+        せいこう: { 読み方: ['成功'], form: '名詞、スル動詞', 意味: "success" },
+        ぐあい: { 読み方: ['具合'], form: '名詞', 意味: "condition; state" },
+        しょうじょう: { 読み方: ['症状'], form: '名詞', 意味: "symptoms" },
+        げんざい: { 読み方: ['現在'], form: '名詞、副詞', 意味: "now; present time; [＝今]" },
+        い: { 読み方: ['胃'], form: '名詞', 意味: "stomach" },
+        おもっくるしい: { 読み方: ['重っ苦しい'], form: 'イ形容詞', 意味: "heavy; dull; leaden" },
+        あびせる: { 読み方: ['浴びせる'], form: '一段動詞、他動詞', 意味: "to shower (someone) with (something); to pour on" },
+        はらをたてる: { 読み方: ['腹を立てる'], form: '表現、一段動詞', 意味: "to get angry[＝おこる]; to take offense; " },
+        むちゃ: { 読み方: ['無茶'], form: 'ナ形容詞', 意味: "unreasonable" },
+        しゅちょう: { 読み方: ['主張する'], form: 'スル動詞', 意味: "to assert; to insist" },
+
+        // : { 読み方: [''], form: '', 意味: "" },
     },
 
     yomerebaii: {
@@ -518,6 +546,27 @@ const kanji = {
         大相撲: { 読み方: ['おおずもう'], form: '名詞', 意味: "the professional sumo" },
         力士: { 読み方: ['りきし'], form: '名詞', 意味: "sumo wrestler" },
         // Chapter 8
+
+        // : { 読み方: [''], form: '', 意味: "" },
+
+        // Chapter 12
+        訴える: { 読み方: ['うったえる'], form: '一段動詞、他動詞', 意味: "to complain of" },
+        吐く: { 読み方: ['はく'], form: '五段動詞、他動詞', 意味: "to throw up" },
+        診る: { 読み方: ['みる'], form: '一段動詞、他動詞', 意味: "to examine (medical); to examine (a patient)" },
+        下痢: { 読み方: ['げり'], form: '名詞、スル動詞', 意味: "diarrhea" },
+        服用: { 読み方: ['ふくよう'], form: '名詞、スル動詞', 意味: "taking medicine [＝薬を飲む]" },
+        軟らかい: { 読み方: ['やわらかい'], form: 'イ形容詞', 意味: "soft" },
+        歯: { 読み方: ['は'], form: '名詞', 意味: "tooth" },
+        歯科: { 読み方: ['しか'], form: '名詞', 意味: "dental clinic; dentistry" },
+        突然: { 読み方: ['とつぜん'], form: 'ノ形容詞、ナ形容詞、副詞', 意味: "suddenly; abrupt; unexpected" },
+        患者: { 読み方: ['かんじゃ'], form: '名詞', 意味: "patient" },
+        問診: { 読み方: ['もんしん'], form: '名詞、スル動詞', 意味: "interview (with a doctor)" },
+        診察: { 読み方: ['しんさつ'], form: '名詞、スル動詞', 意味: "medical examination of a patient" },
+        手順: { 読み方: ['てじゅん'], form: '名詞', 意味: "procedure; process" },
+        雰囲気: { 読み方: ['ふんいき'], form: '名詞', 意味: "atmosphere; mood; aura" },
+        導入: { 読み方: ['どうにゅう'], form: '名詞、スル動詞', 意味: "introduction; bringing in" },
+        脱線: { 読み方: ['だっせん'], form: 'スル動詞', 意味: "to derail; to digress; to ramble" },
+        素人: { 読み方: ['しろうと'], form: '名詞', 意味: "amateur; ordinary person; novice" },
 
         // : { 読み方: [''], form: '', 意味: "" },
     }
