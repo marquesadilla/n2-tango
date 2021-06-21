a6deec8113804aa9d65fdf7ef04a1887215262d8
Marques <MBatoon1996@gmail.com>
2021-06-21 05:48:13 -1000

    ch8 yomu kanji and typo fixes

--- src/Home.js
+++ src/Home.js
@@ -65,7 +65,7 @@ class Home extends React.Component {
 
                 <div className="homeButtonContainer">
                     <Link to={`/kanji?set=${kanjiSetClick}`}><button onClick={this.clickedKanjiSet} value="ch8-kaku" className="btn from-left">第八課書く</button></Link>
-                    <Link to={`/kanji?set=${kanjiSetClick}`}><button disabled onClick={this.clickedKanjiSet} value="ch8-yomerebaii" className="btn from-left">第八課読む</button></Link>
+                    <Link to={`/kanji?set=${kanjiSetClick}`}><button onClick={this.clickedKanjiSet} value="ch8-yomerebaii" className="btn from-left">第八課読む</button></Link>
                 </div>
 
                 <div className="homeButtonContainer">
