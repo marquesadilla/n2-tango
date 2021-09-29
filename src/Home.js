eea384f8cf536a27985a5a820a712a96ff24f892
Marques <MBatoon1996@gmail.com>
2021-09-29 00:25:39 -1000

    added unit 2-1 kanji

--- src/Home.js
+++ src/Home.js
@@ -50,8 +50,14 @@ class Home extends React.Component {
 
                 <div className="homeButtonContainer">
                     <Link to={`/kanji?set=${kanjiSetClick}`}><button disabled onClick={this.clickedKanjiSet} value="u2-tango-1" className="btn from-left">U2単語1</button></Link>
-                    <Link to={`/kanji?set=${kanjiSetClick}`}><button disabled onClick={this.clickedKanjiSet} value="u2-kaku-1" className="btn from-left">U2書き漢字1</button></Link>
-                    <Link to={`/kanji?set=${kanjiSetClick}`}><button disabled onClick={this.clickedKanjiSet} value="u2-yomu-1" className="btn from-right">U2読み漢字1</button></Link>
+                    <Link to={`/kanji?set=${kanjiSetClick}`}><button onClick={this.clickedKanjiSet} value="u2-kaku-1" className="btn from-left">U2書き漢字1</button></Link>
+                    <Link to={`/kanji?set=${kanjiSetClick}`}><button onClick={this.clickedKanjiSet} value="u2-yomu-1" className="btn from-right">U2読み漢字1</button></Link>
+                </div>
+
+                <div className="homeButtonContainer">
+                    <Link to={`/kanji?set=${kanjiSetClick}`}><button disabled onClick={this.clickedKanjiSet} value="u2-tango-2" className="btn from-left">U2単語2</button></Link>
+                    <Link to={`/kanji?set=${kanjiSetClick}`}><button disabled onClick={this.clickedKanjiSet} value="u2-kaku-2" className="btn from-left">U2書き漢字2</button></Link>
+                    <Link to={`/kanji?set=${kanjiSetClick}`}><button disabled onClick={this.clickedKanjiSet} value="u2-yomu-2" className="btn from-right">U2読み漢字2</button></Link>
                 </div>
 
             </React.Fragment>
--- src/chapters/unit2_1.js
+++ src/chapters/unit2_1.js
@@ -4,11 +4,39 @@ const unit2_1 = {
     },
 
     kaku: {
-
+        なかま: { 読み方: ['仲間'], form: '名詞', 意味: "companion; friend; comrade" },
+        ふうけい: { 読み方: ['風景'], form: '名詞', 意味: "scenery" },
+        しゃこうがくしゃ: { 読み方: ['社交学者'], form: '名詞', 意味: "sociologist" },
+        こうきょう: { 読み方: ['公共'], form: '名詞', 意味: "public (facilities, service, etc.)" },
+        よこ: { 読み方: ['横'], form: '名詞', 意味: "horizontal; side-to-side (as opposed to front-to-back)" },
+        うつる: { 読み方: ['映る'], form: '五段動詞、自動詞', 意味: "to be reflected; to be projected; to be displayed" },
+        しんらいせい: { 読み方: ['信頼性'], form: '名詞', 意味: "credibility; reliability" },
+        ほうそく: { 読み方: ['法則'], form: '名詞', 意味: "rule; law" },
+        つうじる: { 読み方: ['通じる'], form: '一段動詞、自動詞', 意味: "to apply; to lead; to get across" },
+        まんいん: { 読み方: ['満員'], form: '名詞', 意味: "full house; no vacancy" },
+        かぐ: { 読み方: ['家具'], form: '名詞', 意味: "furniture" },
+        しゃりょう: { 読み方: ['車両'], form: '名詞', 意味: "railroad car; wheeled vehicle" },
+        かんかく: { 読み方: ['感覚'], form: '名詞、スル動詞', 意味: "sense; sensation; feeling; intuition" },
+        へいき: { 読み方: ['平気'], form: 'ナ形容詞', 意味: "all right; fine; okay" },
+        つくえ: { 読み方: ['机'], form: '名詞', 意味: "desk" },
+        なぞ: { 読み方: ['謎'], form: '名詞', 意味: "mystery" },
+        解く: { 読み方: ['とく'], form: '五段動詞、他動詞', 意味: "to solve; to work out; to answer" },
+        かって: { 読み方: ['勝手'], form: '名詞、ナ形容詞', 意味: "one's own convenience; one's way; selfishness" },
+        ぜんいん: { 読み方: ['全員'], form: '名詞、副詞', 意味: "all members; whole crew; everyone" },
+        きょうゆう: { 読み方: ['共有'], form: '名詞、スル動詞', 意味: "join ownership; sharing (e.g. a viewpoint; files)" },
+        ちょくせつ: { 読み方: ['直接'], form: 'ナ形容詞、名詞', 意味: "direct; immediate; firsthand" },
+        かぎり: { 読み方: ['限り'], form: '動詞＋～限り', 意味: "as long as~" },
     },
 
     yomerebaii: {
-
+        電柱: { 読み方: ['でんちゅう'], form: '名詞', 意味: "utility pole; telephone pole" },
+        植木鉢: { 読み方: ['うえきばち'], form: '名詞', 意味: "potted plant" },
+        化粧: { 読み方: ['けしょう'], form: '名詞、スル動詞', 意味: "make-up" },
+        弁当: { 読み方: ['べんとう'], form: '名詞', 意味: "boxed-lunch" },
+        迷惑: { 読み方: ['めいわく'], form: '名詞、ナ形容詞', 意味: "trouble; bother; annoyance; inconvenience" },
+        被害: { 読み方: ['ひがい'], form: '名詞', 意味: "damage; injury; harm" },
+        及ぶ: { 読み方: ['およぶ'], form: '五段動詞、自動詞', 意味: "to reach" },
+        不愉快: { 読み方: ['ふゆかい'], form: 'ナ形容詞', 意味: "unpleasant" },
     },
 }
 
