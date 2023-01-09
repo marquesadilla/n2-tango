885ffc37b74da6beaf2faaf5ed9b2a43d9169bbc
marquesbatoon@gmail.com <marquesbatoon@gmail.com>
2023-01-08 22:49:39 -1000

    レイアウトに第九章まで加えた

--- src/Home.js
+++ src/Home.js
@@ -38,10 +38,10 @@ class Home extends React.Component {
 
                 <div className="homeButtonContainer">
                     <Link to={`/kanji?set=${kanjiSetClick}`}><button onClick={this.clickedKanjiSet} value="u1-tango-1" className="btn from-left">家族</button></Link>
-                    <Link to={`/kanji?set=${kanjiSetClick}`}><button onClick={this.clickedKanjiSet} value="u1-tango-1" className="btn from-left">友達</button></Link>
-                    <Link to={`/kanji?set=${kanjiSetClick}`}><button onClick={this.clickedKanjiSet} value="u1-tango-1" className="btn from-left">知人・付き合い</button></Link>
-                    <Link to={`/kanji?set=${kanjiSetClick}`}><button onClick={this.clickedKanjiSet} value="u1-tango-1" className="btn from-left">恋人</button></Link>
-                    <Link to={`/kanji?set=${kanjiSetClick}`}><button onClick={this.clickedKanjiSet} value="u1-tango-1" className="btn from-left">関係悪化</button></Link>
+                    <Link to={`/kanji?set=${kanjiSetClick}`}><button disabled onClick={this.clickedKanjiSet} value="u1-tango-1" className="btn from-left">友達</button></Link>
+                    <Link to={`/kanji?set=${kanjiSetClick}`}><button disabled onClick={this.clickedKanjiSet} value="u1-tango-1" className="btn from-left">知人・付き合い</button></Link>
+                    <Link to={`/kanji?set=${kanjiSetClick}`}><button disabled onClick={this.clickedKanjiSet} value="u1-tango-1" className="btn from-left">恋人</button></Link>
+                    <Link to={`/kanji?set=${kanjiSetClick}`}><button disabled onClick={this.clickedKanjiSet} value="u1-tango-1" className="btn from-left">関係悪化</button></Link>
                     {/*
                     <Link to={`/kanji?set=${kanjiSetClick}`}><button onClick={this.clickedKanjiSet} value="u1-kaku-1" className="btn from-left">U1書き漢字1</button></Link>
                     <Link to={`/kanji?set=${kanjiSetClick}`}><button onClick={this.clickedKanjiSet} value="u1-yomu-1" className="btn from-right">U1読み漢字1</button></Link>
@@ -52,11 +52,11 @@ class Home extends React.Component {
                 <h2 className="text-center">暮らし</h2>
                 
                 <div className="homeButtonContainer">
-                    <Link to={`/kanji?set=${kanjiSetClick}`}><button onClick={this.clickedKanjiSet} value="u1-tango-2" className="btn from-left">住まい</button></Link>
-                    <Link to={`/kanji?set=${kanjiSetClick}`}><button onClick={this.clickedKanjiSet} value="u1-tango-2" className="btn from-left">お金</button></Link>
-                    <Link to={`/kanji?set=${kanjiSetClick}`}><button onClick={this.clickedKanjiSet} value="u1-tango-2" className="btn from-left">食事</button></Link>
-                    <Link to={`/kanji?set=${kanjiSetClick}`}><button onClick={this.clickedKanjiSet} value="u1-tango-2" className="btn from-left">買い物</button></Link>
-                    <Link to={`/kanji?set=${kanjiSetClick}`}><button onClick={this.clickedKanjiSet} value="u1-tango-2" className="btn from-left">時を表す言葉</button></Link>
+                    <Link to={`/kanji?set=${kanjiSetClick}`}><button disabled onClick={this.clickedKanjiSet} value="u1-tango-2" className="btn from-left">住まい</button></Link>
+                    <Link to={`/kanji?set=${kanjiSetClick}`}><button disabled onClick={this.clickedKanjiSet} value="u1-tango-2" className="btn from-left">お金</button></Link>
+                    <Link to={`/kanji?set=${kanjiSetClick}`}><button disabled onClick={this.clickedKanjiSet} value="u1-tango-2" className="btn from-left">食事</button></Link>
+                    <Link to={`/kanji?set=${kanjiSetClick}`}><button disabled onClick={this.clickedKanjiSet} value="u1-tango-2" className="btn from-left">買い物</button></Link>
+                    <Link to={`/kanji?set=${kanjiSetClick}`}><button disabled onClick={this.clickedKanjiSet} value="u1-tango-2" className="btn from-left">時を表す言葉</button></Link>
                     {/*
                     <Link to={`/kanji?set=${kanjiSetClick}`}><button onClick={this.clickedKanjiSet} value="u1-kaku-2" className="btn from-left">U1書き漢字2</button></Link>
                     <Link to={`/kanji?set=${kanjiSetClick}`}><button onClick={this.clickedKanjiSet} value="u1-yomu-2" className="btn from-right">U1読み漢字2</button></Link>
@@ -67,15 +67,81 @@ class Home extends React.Component {
                 <h2 className="text-center">家で</h2>
 
                 <div className="homeButtonContainer">
-                    <Link to={`/kanji?set=${kanjiSetClick}`}><button onClick={this.clickedKanjiSet} value="u2-tango-1" className="btn from-left">朝</button></Link>
-                    <Link to={`/kanji?set=${kanjiSetClick}`}><button onClick={this.clickedKanjiSet} value="u2-tango-1" className="btn from-left">日課</button></Link>
-                    <Link to={`/kanji?set=${kanjiSetClick}`}><button onClick={this.clickedKanjiSet} value="u2-tango-1" className="btn from-left">料理</button></Link>
-                    <Link to={`/kanji?set=${kanjiSetClick}`}><button onClick={this.clickedKanjiSet} value="u2-tango-1" className="btn from-left">家事</button></Link>
-                    <Link to={`/kanji?set=${kanjiSetClick}`}><button onClick={this.clickedKanjiSet} value="u2-tango-1" className="btn from-left">引っ越し</button></Link>
+                    <Link to={`/kanji?set=${kanjiSetClick}`}><button disabled onClick={this.clickedKanjiSet} value="u2-tango-1" className="btn from-left">朝</button></Link>
+                    <Link to={`/kanji?set=${kanjiSetClick}`}><button disabled onClick={this.clickedKanjiSet} value="u2-tango-1" className="btn from-left">日課</button></Link>
+                    <Link to={`/kanji?set=${kanjiSetClick}`}><button disabled onClick={this.clickedKanjiSet} value="u2-tango-1" className="btn from-left">料理</button></Link>
+                    <Link to={`/kanji?set=${kanjiSetClick}`}><button disabled onClick={this.clickedKanjiSet} value="u2-tango-1" className="btn from-left">家事</button></Link>
+                    <Link to={`/kanji?set=${kanjiSetClick}`}><button disabled onClick={this.clickedKanjiSet} value="u2-tango-1" className="btn from-left">引っ越し</button></Link>
                     {/*
                     <Link to={`/kanji?set=${kanjiSetClick}`}><button onClick={this.clickedKanjiSet} value="u2-kaku-1" className="btn from-left">U2書き漢字1</button></Link>
                     <Link to={`/kanji?set=${kanjiSetClick}`}><button onClick={this.clickedKanjiSet} value="u2-yomu-1" className="btn from-right">U2読み漢字1</button></Link>
                     */}
+                </div>
+
+                <h1 className="text-center">第四章</h1>
+                <h2 className="text-center">町</h2>
+
+                <div className="homeButtonContainer">
+                    <Link to={`/kanji?set=${kanjiSetClick}`}><button disabled onClick={this.clickedKanjiSet} value="u2-tango-1" className="btn from-left">町</button></Link>
+                    <Link to={`/kanji?set=${kanjiSetClick}`}><button disabled onClick={this.clickedKanjiSet} value="u2-tango-1" className="btn from-left">役所</button></Link>
+                    <Link to={`/kanji?set=${kanjiSetClick}`}><button disabled onClick={this.clickedKanjiSet} value="u2-tango-1" className="btn from-left">ふるさと</button></Link>
+                    <Link to={`/kanji?set=${kanjiSetClick}`}><button disabled onClick={this.clickedKanjiSet} value="u2-tango-1" className="btn from-left">交通</button></Link>
+                    <Link to={`/kanji?set=${kanjiSetClick}`}><button disabled onClick={this.clickedKanjiSet} value="u2-tango-1" className="btn from-left">産業</button></Link>
+                </div>
+
+                <h1 className="text-center">第五章</h1>
+                <h2 className="text-center">学校で</h2>
+
+                <div className="homeButtonContainer">
+                    <Link to={`/kanji?set=${kanjiSetClick}`}><button disabled onClick={this.clickedKanjiSet} value="u2-tango-1" className="btn from-left">学校</button></Link>
+                    <Link to={`/kanji?set=${kanjiSetClick}`}><button disabled onClick={this.clickedKanjiSet} value="u2-tango-1" className="btn from-left">勉強</button></Link>
+                    <Link to={`/kanji?set=${kanjiSetClick}`}><button disabled onClick={this.clickedKanjiSet} value="u2-tango-1" className="btn from-left">試験</button></Link>
+                    <Link to={`/kanji?set=${kanjiSetClick}`}><button disabled onClick={this.clickedKanjiSet} value="u2-tango-1" className="btn from-left">大学・大学院</button></Link>
+                    <Link to={`/kanji?set=${kanjiSetClick}`}><button disabled onClick={this.clickedKanjiSet} value="u2-tango-1" className="btn from-left">パソコン（スマホ）</button></Link>
+                </div>
+
+                <h1 className="text-center">第六章</h1>
+                <h2 className="text-center">会社で</h2>
+
+                <div className="homeButtonContainer">
+                    <Link to={`/kanji?set=${kanjiSetClick}`}><button disabled onClick={this.clickedKanjiSet} value="u2-tango-1" className="btn from-left">就職</button></Link>
+                    <Link to={`/kanji?set=${kanjiSetClick}`}><button disabled onClick={this.clickedKanjiSet} value="u2-tango-1" className="btn from-left">会社</button></Link>
+                    <Link to={`/kanji?set=${kanjiSetClick}`}><button disabled onClick={this.clickedKanjiSet} value="u2-tango-1" className="btn from-left">仕事</button></Link>
+                    <Link to={`/kanji?set=${kanjiSetClick}`}><button disabled onClick={this.clickedKanjiSet} value="u2-tango-1" className="btn from-left">上下関係</button></Link>
+                    <Link to={`/kanji?set=${kanjiSetClick}`}><button disabled onClick={this.clickedKanjiSet} value="u2-tango-1" className="btn from-left">退職・転職</button></Link>
+                </div>
+
+                <h1 className="text-center">第七章</h1>
+                <h2 className="text-center">お気に入り</h2>
+
+                <div className="homeButtonContainer">
+                    <Link to={`/kanji?set=${kanjiSetClick}`}><button disabled onClick={this.clickedKanjiSet} value="u2-tango-1" className="btn from-left">競技</button></Link>
+                    <Link to={`/kanji?set=${kanjiSetClick}`}><button disabled onClick={this.clickedKanjiSet} value="u2-tango-1" className="btn from-left">ファション</button></Link>
+                    <Link to={`/kanji?set=${kanjiSetClick}`}><button disabled onClick={this.clickedKanjiSet} value="u2-tango-1" className="btn from-left">エンターテインメント</button></Link>
+                    <Link to={`/kanji?set=${kanjiSetClick}`}><button disabled onClick={this.clickedKanjiSet} value="u2-tango-1" className="btn from-left">本</button></Link>
+                    <Link to={`/kanji?set=${kanjiSetClick}`}><button disabled onClick={this.clickedKanjiSet} value="u2-tango-1" className="btn from-left">趣味・好み</button></Link>
+                </div>
+
+                <h1 className="text-center">第八章</h1>
+                <h2 className="text-center">お気に入り</h2>
+
+                <div className="homeButtonContainer">
+                    <Link to={`/kanji?set=${kanjiSetClick}`}><button disabled onClick={this.clickedKanjiSet} value="u2-tango-1" className="btn from-left">気候と天気</button></Link>
+                    <Link to={`/kanji?set=${kanjiSetClick}`}><button disabled onClick={this.clickedKanjiSet} value="u2-tango-1" className="btn from-left">台風・地震</button></Link>
+                    <Link to={`/kanji?set=${kanjiSetClick}`}><button disabled onClick={this.clickedKanjiSet} value="u2-tango-1" className="btn from-left">自然</button></Link>
+                    <Link to={`/kanji?set=${kanjiSetClick}`}><button disabled onClick={this.clickedKanjiSet} value="u2-tango-1" className="btn from-left">休日</button></Link>
+                    <Link to={`/kanji?set=${kanjiSetClick}`}><button disabled onClick={this.clickedKanjiSet} value="u2-tango-1" className="btn from-left">旅行</button></Link>
+                </div>
+
+                <h1 className="text-center">第九章</h1>
+                <h2 className="text-center">健康のために</h2>
+
+                <div className="homeButtonContainer">
+                    <Link to={`/kanji?set=${kanjiSetClick}`}><button disabled onClick={this.clickedKanjiSet} value="u2-tango-1" className="btn from-left">体と健康</button></Link>
+                    <Link to={`/kanji?set=${kanjiSetClick}`}><button disabled onClick={this.clickedKanjiSet} value="u2-tango-1" className="btn from-left">病気になる前に</button></Link>
+                    <Link to={`/kanji?set=${kanjiSetClick}`}><button disabled onClick={this.clickedKanjiSet} value="u2-tango-1" className="btn from-left">症状</button></Link>
+                    <Link to={`/kanji?set=${kanjiSetClick}`}><button disabled onClick={this.clickedKanjiSet} value="u2-tango-1" className="btn from-left">病気と治療</button></Link>
+                    <Link to={`/kanji?set=${kanjiSetClick}`}><button disabled onClick={this.clickedKanjiSet} value="u2-tango-1" className="btn from-left">美容</button></Link>
                 </div>
 
                 {/*
--- src/kanji.js
+++ src/kanji.js
@@ -1,6 +1,35 @@
 const kanji = {
     tango: {
+        // ***** 第一章 *****
+        // *** 第一課 ***
         一家: { 読み方: ['いっか'], form: '名詞', 意味: "a family; a household; a home; one's family; whole family" },
+        ありがたい: { 読み方: [''], form: 'い形容詞', 意味: "grateful; thankful" },
+        向き合う: { 読み方: ['むきあう'], form: '五段動詞、自動詞', 意味: "to face; to confront (an issue)" },
+        つくづく: { 読み方: [''], form: 'と副詞', 意味: "deeply; greatly; severely; intently; utterly; completely" },
+        養う: { 読み方: ['やしなう'], form: '五段動詞、他動詞', 意味: "to provide for; to support" },
+        役目: { 読み方: ['やくめ'], form: '名詞', 意味: "duty; role; function" },
+        甘える: { 読み方: ['あまえる'], form: '一段動詞、自動詞', 意味: "to be spoiled; to be dependent; to behaved like a spoiled child; to take advantage of" },
+        世間知らず: { 読み方: ['せけんしらず'], form: '名詞、な形容詞', 意味: "ignorant of the ways of the world; inexperienced" },
+        しつけ: { 読み方: [''], form: '名詞', 意味: "discipline" },
+        言いつける: { 読み方: ['いいつける'], form: '一段動詞、他動詞', 意味: "to tell on (someone); to report; to tell (to do)" },
+        自立: { 読み方: ['じりつ'], form: '名詞、する動詞、自動詞', 意味: "independence; self-reliance" },
+        言い出す: { 読み方: ['いいだす'], form: '五段動詞、他動詞', 意味: "to begin to say; to start talking; to bring up" },
+        意志: { 読み方: ['いし'], form: '名詞', 意味: "intention; will; volition" },
+        尊重: { 読み方: ['そんちょう'], form: '名詞、する動詞、他動詞', 意味: "respect; esteem; regard" },
+        説得: { 読み方: ['せっとく'], form: '名詞、する動詞、他動詞', 意味: "convince; persuasion" },
+        納得: { 読み方: ['なっとく'], form: '名詞、する動詞', 意味: "consent; agreement; acceptance; acknowledgement" },
+        逆らう: { 読み方: ['さからう'], form: '五段動詞、自動詞', 意味: "to go against; to oppose; to disobey" },
+        反省: { 読み方: ['はんせい'], form: '名詞、する動詞、他動詞', 意味: "reflection; reconsideration; contemplation; regret" },
+        放っておく: { 読み方: ['ほうっておく'], form: '表現、五段動詞', 意味: "to leave alone; to leave as is" },
+        介護: { 読み方: ['かいご'], form: '名詞、する動詞、他動詞', 意味: "nursing; caregiving" },
+        世代: { 読み方: ['せだい'], form: '名詞', 意味: "generation" },
+        継ぐ: { 読み方: ['つぐ'], form: '五段動詞、他動詞', 意味: "to inherit; to succeed (a person, to a position, etc.); to take over; to add (e.g. charcoal to the fire)" },
+        妊娠: { 読み方: ['にんしん'], form: '名詞、する動詞、自動詞', 意味: "pregnancy" },
+        出産: { 読み方: ['しゅっさん'], form: '名詞、する動詞', 意味: "childbirth; (giving) birth" },
+        産む: { 読み方: ['うむ'], form: '五段動詞、他動詞', 意味: "to give birth; to lay (eggs)" },
+        // ***** 第一章 *****
+        // *** 第二課
+        
         // : { 読み方: [''], form: '', 意味: "" },
     }
 }
